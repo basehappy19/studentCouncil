8faa974b67a2ac563c49418b0ae568fa946c0847
DESKTOP-EL7LBE9 <pakpoom.td@gmail.com>
2026-06-02 01:41:04 +0700

    update

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,46 +1,51 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/Uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'studentownschool.com',
-        port: '',
-        pathname: '/px/Uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'studentownschool.basehappy19.site',
-        port: '',
-        pathname: '/px/Uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'sc.phukhieo.ac.th',
-        port: '',
-        pathname: '/px/Uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'sc.phukhieo.ac.th',
-        port: '',
-        pathname: '/api/Uploads/**',
-      }
-    ],
-    domains: ['sc.phukhieo.ac.th', 'studentownschool.com', 'studentownschool.basehappy19.site'],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '4mb',
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "127.0.0.1",
+                port: "8000",
+                pathname: "/Uploads/**",
+            },
+            {
+                protocol: "http",
+                hostname: "10.0.4.45",
+                port: "8000",
+                pathname: "/Uploads/**",
+            },
+            {
+                protocol: "https",
+                hostname: "studentownschool.com",
+                port: "",
+                pathname: "/px/Uploads/**",
+            },
+            {
+                protocol: "https",
+                hostname: "studentownschool.basehappy19.site",
+                port: "",
+                pathname: "/px/Uploads/**",
+            },
+            {
+                protocol: "https",
+                hostname: "sc.phukhieo.ac.th",
+                port: "",
+                pathname: "/px/Uploads/**",
+            },
+            {
+                protocol: "https",
+                hostname: "sc.phukhieo.ac.th",
+                port: "",
+                pathname: "/api/Uploads/**",
+            },
+        ],
     },
-  },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "4mb",
+        },
+    },
 };
 
 export default nextConfig;
